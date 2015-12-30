141c2d54385db055623902158be4bf9f5cfe7136
Jesse Liberty <jesseliberty@gmail.com>
2015-12-30 14:29:18 -0500

    Shopping cart class added

---
+++ source/web/store/src/store/Models/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace store.Models
+{
+    public class ShoppingCart
+    {
+        public int ID { get; set; }
+        public int CustomerID { get; set; }
+        public List<OrderDetail> CartItems { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastMOdified { get; set; }
+        public bool Saved { get; set; }
+    }
+}
